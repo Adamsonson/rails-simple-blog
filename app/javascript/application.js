71a52d64f4ec150b174fe4dc165808187b7c80a2
adam somora <adamsomora33@gmail.com>
2023-01-23 00:49:20 +0100

    add JS managing paragraph

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,25 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener('turbolinks:load', () => {
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content')
+    if (!element) return;
+    console.log('gaga');
+
+    element.classList.add('d-none')
+    element.nextElementSibling.classList.remove('d-none')
+  })
+
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+
+    event.target.preventDefault();
+
+    let element = event.target.closest('.paragraph-form')
+
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  })
+})
